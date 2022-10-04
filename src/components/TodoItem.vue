fda135984d1a34af15b150ac063ce5ea727c834e
rennleon <rennleonz@gmail.com>
2022-10-04 16:48:50 -0500

    Create TodoItem component

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="todo-item">
+    <h1>{{ name }}</h1>
+    <small><b>{{ new Date().toISOString() }}</b></small>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: {
+    id: String,
+    name: String,
+    description: String,
+  }
+}
+</script>
+
+<style scoped>
+</style>
