57d81513c234ec3d3548e6cfcc4cd086d4611975
rennleon <rennleonz@gmail.com>
2022-10-04 19:38:02 -0500

    Implement listener for 'onCreateNewTodo' event

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@
 <script>
 import { API } from 'aws-amplify';
 import { listTodos } from './graphql/queries';
+import { onCreateTodo } from './graphql/subscriptions';
 
 import TodoForm from './components/TodoForm.vue'
 import TodoItem from './components/TodoItem.vue'
@@ -30,10 +31,22 @@ export default {
     async fetchTodos() {
       const { data } = await API.graphql({ query: listTodos })
       this.todos = data?.listTodos?.items ?? [];
+    },
+
+    listenNewTodo() {
+      API.graphql({ query: onCreateTodo })
+        .subscribe({
+          next: (evData) => {
+            const newTodo = evData.value.data.onCreateTodo;
+            if (this.todos.find(t => t.id === newTodo.id)) return;
+            this.todos = [newTodo, ...this.todos];
+          }
+        })
     }
   },
 
   beforeMount() {
+    this.listenNewTodo();
     this.fetchTodos();
   }
 }
