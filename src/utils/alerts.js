63ce30f1a00bc0715106a289fa63c226923ccc79
rennleon <rennleonz@gmail.com>
2022-10-04 23:40:00 -0500

    Update alerts to clear before show

--- src/utils/alerts.js
+++ src/utils/alerts.js
@@ -1,29 +1,36 @@
 import toastr from "toastr";
 
 export function alertNewTodo() {
+  toastr.clear();
   toastr.success("Todo created successfully!!", "New todo");
 }
 
 export function alertCreatingTodo() {
+  toastr.clear();
   toastr.info("Creating todo!!", "Processing ...");
 }
 
 export function alertDeletedTodo() {
+  toastr.clear();
   toastr.success("Todo deleted successfully", "Deleted todo");
 }
 
 export function alertDeletingTodo() {
+  toastr.clear();
   toastr.info("Creating todo!!", "Processing ...");
 }
 
 export function alertUpdatedTodo() {
+  toastr.clear();
   toastr.success("Todo updated successfully!!", "Success!!");
 }
 
 export function alertUpdatingTodo() {
+  toastr.clear();
   toastr.info("Updating todo!!", "Processing ...");
 }
 
 export function alertError(msg) {
-  toastr.error(msg, "SOmthing happend!!");
+  toastr.clear();
+  toastr.error(msg, "Error!!");
 }
