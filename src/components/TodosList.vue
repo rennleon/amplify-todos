0e2c56ba8a64c2737f10108552f955602edaf463
rennleon <rennleonz@gmail.com>
2022-10-04 21:22:36 -0500

    Create TodosList component

---
+++ src/components/TodosList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Pending todo's</h1>
+    <ul>
+      <TodoItem
+        v-for="todo in todos"
+        :key="todo.id" 
+        :todo="todo" />
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue'
+
+export default {
+  name: 'TodosList',
+  components: { TodoItem },
+  props: { todos: [] },
+}
+</script>
+
+<style scoped>
+</style>
