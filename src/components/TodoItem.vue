728fa252cb315bd944b5ccc5f1f4c2ef92f42103
rennleon <rennleonz@gmail.com>
2022-10-04 21:28:17 -0500

    Register parseDate in TodoItem component

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -2,7 +2,7 @@
   <li class="todo-item">
     <h1>{{ todo.name }}</h1>
     <p>{{ todo.description }}</p>
-    <small><b>{{ todo.createdAt }}</b></small>
+    <small><b>created at:</b> {{ parseDate(todo.createdAt) }}</small>
 
     <input
       ref="doneCheckbox"
@@ -22,11 +22,14 @@
 <script>
 import { API } from 'aws-amplify';
 import { updateTodo, deleteTodo } from '../graphql/mutations';
+import { parseDate } from '../utils/date-utils.js'
 
 export default {
   name: 'TodoItem',
   props: { todo: Object },
   methods: {
+    parseDate,
+
     onDelete() {
       this.$refs.deleteBtn.disabled = true;
       API.graphql({
