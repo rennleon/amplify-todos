225b477f3d8caf5139045717398fbf024d106f42
rennleon <rennleonz@gmail.com>
2022-10-04 19:47:43 -0500

    Update component template & implement delete fucntionality

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,15 +1,28 @@
 <template>
-  <div class="todo-item">
+  <li class="todo-item">
     <h1>{{ todo.name }}</h1>
+    <p>{{ todo.description }}</p>
     <small><b>{{ todo.createdAt }}</b></small>
     <input type="checkbox" :checked="todo.done" />
-  </div>
+    <button @click="onDelete">delete</button>
+  </li>
 </template>
 
 <script>
+import { API } from 'aws-amplify';
+import { deleteTodo } from '../graphql/mutations';
+
 export default {
   name: 'TodoItem',
-  props: { todo: Object }
+  props: { todo: Object },
+  methods: {
+    onDelete() {
+      API.graphql({
+        query: deleteTodo,
+        variables: { input: { id: this.todo.id } }
+      })
+    }
+  }
 }
 </script>
 
